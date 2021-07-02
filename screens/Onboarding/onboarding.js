e7a7e03b1e004d8f4954da7d0d17cceb87992c22
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-02 12:46:13 +0100

    feat: complete onboarding screen

---
+++ screens/Onboarding/onboarding.js
@@ -0,0 +1,52 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import * as Colors from "../../common/colors";
+import * as Images from "../../assets/images";
+import { Button } from "../../components/buttons";
+
+export default function Onboarding({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style={"light"} />
+      <Text style={styles.findGadget}>Find your Gadget</Text>
+      <Image
+        source={Images.Splash}
+        resizeMode={"contain"}
+        style={styles.splashImg}
+      />
+      <Button
+        title="Get started"
+        style={styles.button}
+        titleColor
+        onPress={() => navigation.navigate("login")}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.Purple,
+    paddingTop: 40,
+  },
+  findGadget: {
+    fontSize: 60,
+    fontWeight: "800",
+    color: Colors.White,
+    marginTop: 30,
+    alignSelf: "center",
+    width: 290,
+    letterSpacing: 0.8,
+  },
+  splashImg: {
+    width: "100%",
+    height: 450,
+  },
+  button: {
+    backgroundColor: Colors.White,
+    marginTop: 50,
+    width: 300,
+  },
+});
