826d3782341afc8340b11bf2cc3faa12ab2daa4b
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-02 12:47:13 +0100

    chore: create reusable button

---
+++ components/buttons.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { StyleSheet, TextInput, TouchableOpacity, Text } from "react-native";
+import * as Colors from "../common/colors";
+
+export const Button = ({
+  title,
+  titleColor,
+  isSmall,
+  style,
+  hasBorder,
+  onPress,
+}) => {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.account,
+        isSmall && styles.smallButton,
+        titleColor && styles.titleColorButton,
+        hasBorder && styles.borderButton,
+        style,
+      ]}
+      onPress={onPress}
+    >
+      <Text style={[styles.acct, titleColor && styles.titleColor]}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  borderButton: {
+    backgroundColor: "transparent",
+    borderWidth: 2,
+    borderColor: "white",
+  },
+  titleColorButton: {
+    backgroundColor: "white",
+  },
+  smallButton: {
+    width: 133,
+  },
+  titleColor: {
+    color: Colors.Purple,
+  },
+  acct: {
+    fontWeight: "700",
+    color: "white",
+    fontSize: 19,
+  },
+  account: {
+    backgroundColor: Colors.Purple,
+    height: 65,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+    width: 320,
+    alignSelf: "center",
+  },
+});
