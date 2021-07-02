872b930bc579ad4ae16de4e2bef07c65eea6ab13
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-02 12:47:53 +0100

    chore: set up colors file

---
+++ common/colors.js
@@ -0,0 +1,6 @@
+export const Purple = "#5956E9";
+export const White = "#FFFFFF";
+export const Black = "#000000";
+export const Gray = "#C9C9C9";
+export const Grey = "#868686";
+export const Ash = "#9A9A9D";
