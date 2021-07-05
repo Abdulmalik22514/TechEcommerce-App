b26bb7cb57f9a208ac90e81f0af9267cbbb236f4
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-05 22:08:07 +0100

    chore: add screens

--- navigation.js
+++ navigation.js
@@ -4,6 +4,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Onboarding from "./screens/Onboarding/onboarding";
 import Login from "./screens/Login/login";
 import Home from "./screens/Home/home";
+import SearchResult from "./screens/SearchResults/search";
+import NoResult from "./screens/SearchResults/noResult";
+import Details from "./screens/Details/details";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +17,9 @@ export default function Navigator() {
         <Stack.Screen name="onboard" component={Onboarding} />
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="search" component={SearchResult} />
+        <Stack.Screen name="noresult" component={NoResult} />
+        <Stack.Screen name="details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
