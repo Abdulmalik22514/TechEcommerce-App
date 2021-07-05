d473cbfa538c8913558b3429feb101188764170a
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-05 22:07:38 +0100

    chore: set up font

--- App.js
+++ App.js
@@ -1,7 +1,46 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigator from "./navigation";
+import { View, Text, StyleSheet } from "react-native";
+
+import * as Font from "expo-font";
 
 export default function App() {
-  return <Navigator />;
+  const [state, setState] = useState(true);
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      Montserrat: require("./assets/fonts/Montserrat.ttf"),
+    });
+    setState(true);
+  }
+
+  // useEffect(() => {
+  //   loadFonts();
+  // }, []);
+
+  if (state) {
+    return <Navigator />;
+  } else return null;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+//   const [loaded] = useFonts({
+//     Montserrat: require("./assets/fonts/Montserrat.ttf"),
+//   });
+//   console.log(loaded);
+
+//   if (!loaded) {
+//     return null;
+//   }
+
+//   return <Navigator />;
+// }
