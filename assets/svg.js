714fd30efc98eb045e8d96a334be17ce444a836b
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-05 22:08:41 +0100

    chore: add svg icon and images

--- assets/svg.js
+++ assets/svg.js
@@ -234,3 +234,47 @@ export const Seemore = (props) => {
     </Svg>
   );
 };
+
+export const BackArrow = (props) => {
+  return (
+    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+      <Path
+        d="M4.25 12.2744L19.25 12.2744"
+        stroke="#200E32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M10.2998 18.2988L4.2498 12.2748L10.2998 6.24976"
+        stroke="#200E32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
+
+export const Heart = (props) => {
+  return (
+    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+      <Path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.761 20.8538C9.5904 19.5179 7.57111 17.9456 5.73929 16.1652C4.45144 14.8829 3.47101 13.3198 2.8731 11.5954C1.79714 8.25031 3.05393 4.42083 6.57112 3.28752C8.41961 2.69243 10.4384 3.03255 11.9961 4.20148C13.5543 3.03398 15.5725 2.69398 17.4211 3.28752C20.9383 4.42083 22.2041 8.25031 21.1281 11.5954C20.5302 13.3198 19.5498 14.8829 18.2619 16.1652C16.4301 17.9456 14.4108 19.5179 12.2402 20.8538L12.0051 21L11.761 20.8538Z"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M15.7394 7.05298C16.8047 7.39328 17.5616 8.34968 17.6562 9.47496"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
