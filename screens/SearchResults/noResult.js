3f33706ea4dd3c365571169ce7580e587331da0a
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-05 23:59:53 +0100

    chore: refactor noresult screen

--- screens/SearchResults/noResult.js
+++ screens/SearchResults/noResult.js
@@ -3,24 +3,27 @@ import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import * as Colors from "../../common/colors";
 import { SearchBox } from "./utils/searchBox";
 import * as Images from "../../assets/images";
+import { Container } from "../../common/container";
 
 export default function NoResult({ navigation }) {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <SearchBox placeholder="Apppi" onPress={() => navigation.pop()} />
-        <Image
-          source={Images.NotFound}
-          style={styles.NotFound}
-          resizeMode={"cover"}
-        />
-        <Text style={styles.result}>Item not found</Text>
-        <Text style={styles.tryMore}>
-          Try a more generic search term or try looking for alternative
-          products.
-        </Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <Container>
+        <View style={{ paddingHorizontal: 25 }}>
+          <SearchBox placeholder="Apppi" onPress={() => navigation.pop()} />
+          <Image
+            source={Images.NotFound}
+            style={styles.NotFound}
+            resizeMode={"cover"}
+          />
+          <Text style={styles.result}>Item not found</Text>
+          <Text style={styles.tryMore}>
+            Try a more generic search term or try looking for alternative
+            products.
+          </Text>
+        </View>
+      </Container>
+    </>
   );
 }
 
