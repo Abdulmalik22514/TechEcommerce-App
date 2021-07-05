69d48695a21e615e05b7a9404df5327c38b53680
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-05 23:09:06 +0100

    chore: refactor onboarding screen

--- screens/Onboarding/onboarding.js
+++ screens/Onboarding/onboarding.js
@@ -4,24 +4,26 @@ import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import * as Colors from "../../common/colors";
 import * as Images from "../../assets/images";
 import { Button } from "../../components/buttons";
+import { Container } from "../../common/container";
 
 export default function Onboarding({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style={"light"} />
-      <Text style={styles.findGadget}>Find your Gadget</Text>
-      <Image
-        source={Images.Splash}
-        resizeMode={"contain"}
-        style={styles.splashImg}
-      />
-      <Button
-        title="Get started"
-        style={styles.button}
-        titleColor={Colors.Purple}
-        onPress={() => navigation.navigate("login")}
-      />
-    </SafeAreaView>
+    <>
+      <Container barColor={Colors.Purple} backgroundColor={Colors.Purple}>
+        <Text style={styles.findGadget}>Find your Gadget</Text>
+        <Image
+          source={Images.Splash}
+          resizeMode={"contain"}
+          style={styles.splashImg}
+        />
+        <Button
+          title="Get started"
+          style={styles.button}
+          titleColor={Colors.Purple}
+          onPress={() => navigation.navigate("login")}
+        />
+      </Container>
+    </>
   );
 }
 
