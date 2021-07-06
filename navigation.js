a09a1c1fe32fc92da1fd51a14e7279da46da0d02
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-06 16:12:16 +0100

    chore: add new screen

--- navigation.js
+++ navigation.js
@@ -4,9 +4,10 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Onboarding from "./screens/Onboarding/onboarding";
 import Login from "./screens/Login/login";
 import Home from "./screens/Home/home";
-import SearchResult from "./screens/SearchResults/search";
-import NoResult from "./screens/SearchResults/noResult";
 import Details from "./screens/Details/details";
+import SearchResult from "./screens/SearchResults/Search/search";
+import NoResult from "./screens/SearchResults/NoResult/noResult";
+import Basket from "./screens/Basket/basket";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function Navigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode={"none"}>
+        <Stack.Screen name="basket" component={Basket} />
         <Stack.Screen name="onboard" component={Onboarding} />
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="home" component={Home} />
