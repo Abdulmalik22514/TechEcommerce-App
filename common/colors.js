cc22f3b25cbad13bc0d375db022e1d4630bff04a
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-06 14:53:30 +0100

    chore: add colors

--- common/colors.js
+++ common/colors.js
@@ -5,3 +5,7 @@ export const Gray = "#C9C9C9";
 export const Grey = "#868686";
 export const Ash = "#9A9A9D";
 export const Background = "#F2F2F2";
+export const Border = "#E3E3E3";
+export const SkyBlue = "#7485C1";
+export const RoseGold = "#C9A19C";
+export const Green = "#A1C89B";
