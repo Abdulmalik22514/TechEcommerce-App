f34e91ffe4c73f3a77f9c06392f3beafe11dd27e
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-09 10:06:52 +0100

    chore: add basket screen

--- navigation.js
+++ navigation.js
@@ -15,13 +15,13 @@ export default function Navigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode={"none"}>
-        <Stack.Screen name="basket" component={Basket} />
         <Stack.Screen name="onboard" component={Onboarding} />
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="home" component={Home} />
         <Stack.Screen name="search" component={SearchResult} />
         <Stack.Screen name="noresult" component={NoResult} />
         <Stack.Screen name="details" component={Details} />
+        <Stack.Screen name="basket" component={Basket} />
       </Stack.Navigator>
     </NavigationContainer>
   );
